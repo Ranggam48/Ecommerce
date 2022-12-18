99e2299535190699316cd02edd2a6776b5bd5123
ranggam48 <ranggam48@gmail.com>
2022-12-18 14:28:09 +0700

    membuat model dengan sequelize

---
+++ migrations/20221026140054-create-order.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      shoppingCartId: {
+        type: Sequelize.STRING
+      },
+      customerAddress: {
+        type: Sequelize.STRING
+      },
+      paymentMethod: {
+        type: Sequelize.INTEGER
+      },
+      statusPayment: {
+        type: Sequelize.INTEGER
+      },
+      confirmPayment: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
